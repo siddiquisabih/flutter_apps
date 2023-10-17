ea38b93542781bcbad6600aa5acc42e7faf9464c
siddiquisabih <siddiquisabih94@gmail.com>
2023-10-17 14:38:45 +0500

    theme customization and dark, light theme sample

---
+++ use_flutter_theme/android/app/src/main/kotlin/com/example/use_flutter_theme/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.use_flutter_theme
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ use_flutter_theme/lib/theme/color_constant.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const PRIMARY_COLOR = Colors.deepOrangeAccent;
+const ACCENT_COLOR = Colors.orange;
