7bf9f7f87572a041792e75202925d2d9b0987bfc
siddiquisabih <siddiquisabih94@gmail.com>
2023-10-20 15:59:54 +0500

    modify storage and fix bugs

--- local_storage/lib/storage_controller.dart
+++ local_storage/lib/storage_controller.dart
@@ -3,13 +3,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 class StorageController {
   final String _storageKey = "userDataKey";
 
-  // save data in local storage
+  // save data in local storage (phone storage)
   void saveInStorage(String textValue) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString(_storageKey, textValue);
   }
 
-  // get data from local storage
+  // get data from local storage (phone storage)
   Future<String> getFromStorage() async {
     final storage = await SharedPreferences.getInstance();
     final detail = storage.getString(_storageKey) ?? '';
