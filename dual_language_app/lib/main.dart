daa48eb05bd702d2f44cbf9d9c98a915fea0e2d7
siddiquisabih <siddiquisabih94@gmail.com>
2023-11-02 16:27:32 +0500

    added translation page and translation json file

--- dual_language_app/lib/main.dart
+++ dual_language_app/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_locales/flutter_locales.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Locales.init(["en", 'ur']);
   runApp(const MyApp());
 }
 
@@ -10,24 +13,95 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return LocaleBuilder(
+      builder: (locale) => MaterialApp(
+        localizationsDelegates: Locales.delegates,
+        supportedLocales: Locales.supportedLocales,
+        locale: locale,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Text("asdasdsd")),
+    return Scaffold(
+      appBar: AppBar(
+        title: const LocaleText(
+          "language_app",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.orange,
+      ),
+      body: const SafeArea(
+        child: Center(
+          child: LocaleText(
+            "welcome",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => const LanguageChange(),
+          ),
+        ),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class LanguageChange extends StatefulWidget {
+  const LanguageChange({super.key});
+
+  @override
+  State<LanguageChange> createState() => _LanguageChangeState();
+}
+
+class _LanguageChangeState extends State<LanguageChange> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // title: LocaleText(
+        //   "Language",
+        //   style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        // ),
+        title: const LocaleText(
+          "language_app",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.orange,
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            title: const Text("English"),
+            onTap: () => LocaleNotifier.of(context)!.change("en"),
+          ),
+          ListTile(
+            title: const Text("زبان"),
+            onTap: () => LocaleNotifier.of(context)!.change("ur"),
+          )
+        ],
+      ),
     );
   }
 }
